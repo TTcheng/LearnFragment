4e93de96c217624739f1d694e831b9c49262b206
Wang Chuncheng <TTChengWang@foxmail.com>
2017-10-19 14:29:16 +0800

    Initial commit

---
+++ app/src/main/java/com/example/wangchuncheng/learnfragment/BlankFragment.java
@@ -0,0 +1,42 @@
+package com.example.wangchuncheng.learnfragment;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class BlankFragment extends Fragment {
+
+    public BlankFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, final ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView =  inflater.inflate(R.layout.fragment_blank, container, false);
+        rootView.findViewById(R.id.btnShowAnotherFragment).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getFragmentManager().beginTransaction()
+                        .addToBackStack(null)
+                        .replace(R.id.activity_main,new AnotherFragment())
+                        .commit();
+            }
+        });
+        rootView.findViewById(R.id.btnStartSliderActivity).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(),SliderActivity.class));
+            }
+        });
+        return rootView;
+    }
+}
